cbafd8d5a72670bf90f90317c76732c3d904185d
helil-primetech <84941363+helil-primetech@users.noreply.github.com>
2021-06-03 22:46:55 -0400

    Update AmazonSearchTest.java
    code updated

--- src/test/java/tests/AmazonSearchTest.java
+++ src/test/java/tests/AmazonSearchTest.java
@@ -11,15 +11,38 @@ import org.testng.annotations.AfterMethod;
 
 public class AmazonSearchTest {
 	
-	AmazonHomePage amazonHP = new AmazonHomePage();
+	AmazonHomePage amazonHP;
 	
 	
   @Test
   public void amazonSearchTest() throws InterruptedException {
 	  Driver.getDriver().get(PropertiesReader.getProperty("amazonURL"));
+	  amazonHP = new AmazonHomePage();
 	  amazonHP.searchBox.sendKeys("coffee mug");
 	  amazonHP.searchButton.click();
 	  Thread.sleep(4000);
+  }
+  
+  @Test
+  public void dropDownTest() {
+	  Driver.getDriver().get(PropertiesReader.getProperty("amazonURL"));
+	  amazonHP = new AmazonHomePage();
+	  BrowserUtils.selectByVisibleText(amazonHP.departmentsDropdown, "Amazon Fresh");
+  }
+  
+  @Test(dependsOnMethods = "amazonSearchTest")
+  public void searchResultVerify() throws InterruptedException {
+	  Driver.getDriver().get(PropertiesReader.getProperty("amazonURL"));
+	  amazonHP = new AmazonHomePage();
+	  amazonHP.searchBox.sendKeys("coffee mug");
+	  amazonHP.searchButton.click();
+	  Thread.sleep(3000);
+	  System.out.println("Number of items : " + amazonHP.searchItemPrices.size());
+	  
+	  for (WebElement itemPrice : amazonHP.searchItemPrices) {
+		String price = itemPrice.getText();
+		  System.out.println(price);
+	}
   }
   
   @BeforeMethod
