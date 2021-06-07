50faca7dff988ad23bef65e1b259f13775e1ae1a
helil-primetech <helil@primetechschool.com>
2021-06-06 22:18:04 -0400

    removed the target folder

--- src/test/java/tests/AmazonSearchTest.java
+++ src/test/java/tests/AmazonSearchTest.java
@@ -20,83 +20,116 @@ public class AmazonSearchTest {
 	BrowserUtils utils = new BrowserUtils();
 	
 	
-  @Test
-  public void amazonSearchTest() throws InterruptedException {
-	  Driver.getDriver().get(PropertiesReader.getProperty("amazonURL"));
-	  amazonHP = new AmazonHomePage();
-	  amazonHP.searchBox.sendKeys("coffee mug");
-	  amazonHP.searchButton.click();
-	  Thread.sleep(4000);
-  }
+//  @Test
+//  public void amazonSearchTest() throws InterruptedException {
+//	  Driver.getDriver().get(PropertiesReader.getProperty("amazonURL"));
+//	  amazonHP = new AmazonHomePage();
+//	  amazonHP.searchBox.sendKeys("coffee mug");
+//	  amazonHP.searchButton.click();
+//	  Thread.sleep(4000);
+//  }
   
-  @Test
-  public void dropDownTest() {
-	  Driver.getDriver().get(PropertiesReader.getProperty("amazonURL"));
-	  amazonHP = new AmazonHomePage();
-	  BrowserUtils.selectByVisibleText(amazonHP.departmentsDropdown, "Amazon Fresh");
-  }
-  
-  @Test(dependsOnMethods = "amazonSearchTest")
-  public void searchResultVerify() throws InterruptedException {
-	  Driver.getDriver().get(PropertiesReader.getProperty("amazonURL"));
-	  amazonHP = new AmazonHomePage();
-	  amazonHP.searchBox.sendKeys("coffee mug");
-	  amazonHP.searchButton.click();
-	  Thread.sleep(3000);
-	  System.out.println("Number of items : " + amazonHP.searchItemPrices.size());
-	  
-	  for (WebElement itemPrice : amazonHP.searchItemPrices) {
-		String price = itemPrice.getText();
-		  System.out.println(price);
-	}
-	  System.out.println("Thread id " + Thread.currentThread().getId());
-  }
+//  @Test
+//  public void dropDownTest() {
+//	  Driver.getDriver().get(PropertiesReader.getProperty("amazonURL"));
+//	  amazonHP = new AmazonHomePage();
+//	  BrowserUtils.selectByVisibleText(amazonHP.departmentsDropdown, "Amazon Fresh");
+//  }
+//  
+//  @Test(dependsOnMethods = "amazonSearchTest")
+//  public void searchResultVerify() throws InterruptedException {
+//	  Driver.getDriver().get(PropertiesReader.getProperty("amazonURL"));
+//	  amazonHP = new AmazonHomePage();
+//	  amazonHP.searchBox.sendKeys("coffee mug");
+//	  amazonHP.searchButton.click();
+//	  Thread.sleep(3000);
+//	  System.out.println("Number of items : " + amazonHP.searchItemPrices.size());
+//	  
+//	  for (WebElement itemPrice : amazonHP.searchItemPrices) {
+//		String price = itemPrice.getText();
+//		  System.out.println(price);
+//	}
+//	  System.out.println("Thread id " + Thread.currentThread().getId());
+//  }
   
  // scenario:  go to amazon and perform a series of search tests based on the search criteria provided. 
  // verify the search criteria is displayed on the search result page. 
   
   
-  @Test(dataProvider = "searchTestData")
-  public void searchTest(String input) {
-	  Driver.getDriver().get(PropertiesReader.getProperty("amazonURL"));
-	  amazonHP = new AmazonHomePage();
-	  Assert.assertTrue(amazonHP.searchBox.isDisplayed());
-	  amazonHP.searchBox.sendKeys(input);
-	  amazonHP.searchButton.click();
-	  utils.waitUntilElementIsVisible(amazonHP.searchCriteriaText);
-	  String searchText = amazonHP.searchCriteriaText.getText();
-	  System.out.println(searchText);
-	  String afterSubString = input.substring(0, input.length());
-	  Assert.assertEquals(afterSubString, input);
-	  System.out.println("Thread id " + Thread.currentThread().getId());
+//  @Test(dataProvider = "searchTestData")
+//  public void searchTest(String input) {
+//	  Driver.getDriver().get(PropertiesReader.getProperty("amazonURL"));
+//	  amazonHP = new AmazonHomePage();
+//	  Assert.assertTrue(amazonHP.searchBox.isDisplayed());
+//	  amazonHP.searchBox.sendKeys(input);
+//	  amazonHP.searchButton.click();
+//	  utils.waitUntilElementIsVisible(amazonHP.searchCriteriaText);
+//	  String searchText = amazonHP.searchCriteriaText.getText();
+//	  System.out.println(searchText);
+//	  String afterSubString = input.substring(0, input.length());
+//	  Assert.assertEquals(afterSubString, input);
+//	  System.out.println("Thread id " + Thread.currentThread().getId());
+//  }
+//  
+//  
+//  @DataProvider
+//  public String[] searchTestData(){
+//	  String[] testData = new String[4];
+//	  
+//	  testData[0] = "Coffee mug";
+//	  testData[1] = "Pretty Coffee mug";
+//	  testData[2] = "Cool Coffee mug";
+//	  testData[3] = "Amazing Coffee mug";
+//	  
+////	  testData[0][0] = "Coffee mug";
+////	  testData[0][0] = "Coffee mug";
+////	  testData[0][0] = "Coffee mug";
+////	  testData[0][0] = "Coffee mug";
+//	  
+//	  return testData;
+//  }
+  
+  
+  @Test(dataProvider="searchTest")
+  public void testTest(String input, String location) {
+	  System.out.println(input +" : " + location);
   }
   
   
   @DataProvider
-  public String[] searchTestData(){
-	  String[] testData = new String[4];
-	  
-	  testData[0] = "Coffee mug";
-	  testData[1] = "Pretty Coffee mug";
-	  testData[2] = "Cool Coffee mug";
-	  testData[3] = "Amazing Coffee mug";
+  public String[][] searchTest(){
+	  //Object[][] testData = new Object[4][4];
 	  
 //	  testData[0][0] = "Coffee mug";
-//	  testData[0][0] = "Coffee mug";
-//	  testData[0][0] = "Coffee mug";
-//	  testData[0][0] = "Coffee mug";
+//	  testData[0][1] = "DC";
+//	  
+//	  testData[1][0] = "Cool Coffee mug";
+//	  testData[1][1] = "Chicago";
+//	  
+//	  testData[2][0] = "Coffee mug";
+//	  testData[2][1] = "LA";
+//	  
+//	  testData[3][0] = "Cool Coffee mug";
+//	  testData[3][1] = "Reston";
 	  
-	  return testData;
+	  return new String[][] 
+		{
+		  {"Coffee mug", "DC"},
+		  {"cool Coffee mug", "chicago"},
+		  {"cute Coffee mug", "LA"},
+		  {"ugly Coffee mug", "Reston"}
+		  
+		};
   }
-  
-  @BeforeMethod
-  public void beforeMethod() {
-	  Driver.getDriver();
-  }
-
-  @AfterMethod
-  public void afterMethod() {
-	  Driver.quitDriver();
-  }
+//  
+//  @BeforeMethod
+//  public void beforeMethod() {
+//	  Driver.getDriver();
+//  }
+//
+//  @AfterMethod
+//  public void afterMethod() {
+//	  Driver.quitDriver();
+//  }
 
 }
