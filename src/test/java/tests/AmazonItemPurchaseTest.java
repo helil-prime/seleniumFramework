bee93fcdfcf612f9e527549b28e962310fd802c5
helil-primetech <84941363+helil-primetech@users.noreply.github.com>
2022-01-21 21:46:29 -0500

    Update AmazonItemPurchaseTest.java
    updated

--- src/test/java/tests/AmazonItemPurchaseTest.java
+++ src/test/java/tests/AmazonItemPurchaseTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 public class AmazonItemPurchaseTest {
   @Test
   public void f() {
-	  System.out.println("This is purchase item tests.");
+	  System.out.println("This is purchase item test on Thread : " + Thread.currentThread().getId());
+	  Driver.getDriver().get("https://indeed.com");
   }
 }
